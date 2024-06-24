fda09797676ba2af393764e8c59a12c7f5b5ee2a
ryiing <ryanying1102@gmail.com>
2024-06-24 13:39:03 -0400

    structure finished

--- javascript.js
+++ javascript.js
@@ -13,7 +13,6 @@ const board = (() => {
     const setCell = (index, player) => {
         if(board[index] == '') {
             board[index] = player.marker;
-            gameController.switchPlayer();
         }
         else {
             return;
@@ -24,53 +23,90 @@ const board = (() => {
         return board[index];
     };
 
+    const reset = () => {
+        for(let i = 0; i < board.length; i++) {
+            board[i] = '';
+        }
+    }  
+
     return {
-        getBoard, setCell, getCell
+        getBoard, setCell, getCell, reset
     };
 })();
 
  const gameController = (() => {
     let player1 = createPlayer('Ryan', 'x');
     let player2 = createPlayer('Moeka', 'o');
+    let isOver = false;
+    let round = 0;
 
     let activePlayer = player1;
 
     const getActivePlayer = () => {
         return activePlayer;
-    }
+    };
 
     const switchPlayer = () => {
         activePlayer = activePlayer === player1 ? player2 : player1;
-        console.log('player switched');
     };
 
     const playRound = (index) => {
+        console.log(round);
         console.log('Marking cell');
         board.setCell(index, activePlayer);
+        if(checkWin()) {
+            isOver = true;
+            displayController.updateWinner(activePlayer.name);
+            return;
+        }
+        if (round == 8) {
+            isOver = true;
+            displayController.updateTie();
+            return;
+        }
+        else {
+            switchPlayer();
+            displayController.updateMessage(activePlayer.name);
+        };
+        round++;
     }
 
-    const printNewRound = () => {
-        console.log(`now it is ${activePlayer.name}'s turn!`);
-    }
+    const isGameOver = () => {
+        return isOver;
+    };
 
     const checkWin = () => {
+        const currentBoard = board.getBoard();
         const winCondition = [
-            [0,1,2],
-            [3,4,5],
-            [6,7,8],
-            [0,3,6],
-            [1,4,7],
-            [2,5,8],
-            [0,4,8],
-            [2,4,6]
-        ]
-        
+            [0, 1, 2], //rows
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6], //columns
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8], //diagnols
+            [2, 4, 6],
+        ];
+        for (let condition of winCondition) {
+            const [a, b, c] = condition;
+            if(currentBoard[a] !== "" && currentBoard[a] == currentBoard[b] && currentBoard[b] == currentBoard[c]){
+                return true;
+            }
+        }
+    };
 
-        
+    const reset = () => {
+        activePlayer = player1;
+        isOver = false;
+        round = 0;
+    }
+
+    const getRound = () => {
+        return round;
     }
 
     return { 
-        playRound, getActivePlayer,switchPlayer
+        playRound, getActivePlayer,switchPlayer, checkWin, isGameOver, getRound, reset
     }
 })();
 
@@ -82,25 +118,65 @@ const displayController = (() => {
     const updateScreen = (() => {
         gameBoardDiv.textContent = ''; //clear board
         const screenboard = board.getBoard();
-        const currentPlayer = gameController.getActivePlayer();
-
-        playerTurnDiv.textContent = `Now it is ${currentPlayer.name}'s turn...`
 
         screenboard.forEach((e, index) => {
             const cellButton = document.createElement('button');
             cellButton.classList.add("cell");
             cellButton.textContent = board.getCell(index);
             cellButton.addEventListener('click', () => {
-                board.setCell(index, currentPlayer);
-                gameController.playRound(index);
-                updateScreen();
+                if (gameController.isGameOver() == true || cellButton.textContent !== ''){
+                    return;
+                }
+                else {
+                    gameController.playRound(index);
+                    updateScreen();
+                }
             });
-
             gameBoardDiv.appendChild(cellButton);
         });
 
+        resetButton.addEventListener('click', () => {
+            reset();
+        });
 
     });
 
+    const updateMessage = (text) => {
+        playerTurnDiv.textContent = `Now it is ${text}'s turn`;
+    }
+
+    const updateWinner = (text) => {
+        playerTurnDiv.textContent = `The winner is ${text}`;
+    }
+
+    const resetMessage = () => {
+        playerTurnDiv.textContent = `Ryan Starts with X!`;
+    }
+
+    const updateTie = () => {
+        playerTurnDiv.textContent = 'Its a tie!';
+    }
+
+    const reset = () => {
+        console.log('reset!');
+        board.reset();
+        gameController.reset();
+        resetMessage();
+        updateBoard();
+    }
+
+    const updateBoard = () => {
+        cells = document.getElementsByClassName("cell");
+        for(let i=0; i < cells.length; i++){
+            cells[i].textContent = board.getCell(i);
+        }
+    };
+
+    // currentPlayer = gameController.getActivePlayer();
+    // playerTurnDiv.textContent = `${currentPlayer.name} starts with X!`;
     updateScreen();
+
+    return {
+        updateMessage, updateWinner, updateTie
+    }
 })();
